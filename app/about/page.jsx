5ec0b751e2cbe248a8831d27fae03c4aece66cd7
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-25 19:01:10 +0545

    day out

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function page() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <h1 className="text-6xl"> This is about page</h1>
+      <h1 className="text-6xl">
+        <i>This is about page</i>
+      </h1>
     </div>
   );
 }
