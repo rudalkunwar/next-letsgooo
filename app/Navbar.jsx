e9a00b8159c3c5ae047917c5bbd4d1108f528c59
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-25 18:43:25 +0545

    navbar added

---
+++ app/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Navbar() {
+  return (
+      <ul className="flex justify-center px-2 py-3 bg-blue-500">
+        <li className="px-2">Home</li>
+        <li className="px-2">About</li>
+        <li className="px-2">Contact</li>
+        <li className="px-2">Login</li>
+      </ul>
+  );
+}
+
+export default Navbar;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 function page() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-6xl">This is my next app lets goooooooooo</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="h-screen flex justify-center items-center">
+        <h1 className="text-6xl">This is my next app lets goooooooooo</h1>
+      </div>
+    </>
   );
 }
 
