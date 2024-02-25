6edbfe4f414e1c4d10393cd31c5318ff2225ad7f
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-25 18:51:44 +0545

    littlle routing

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -1,12 +1,20 @@
+import Link from "next/link";
 import React from "react";
 function Navbar() {
   return (
-      <ul className="flex justify-center px-2 py-3 bg-blue-500">
-        <li className="px-2">Home</li>
-        <li className="px-2">About</li>
-        <li className="px-2">Contact</li>
-        <li className="px-2">Login</li>
-      </ul>
+    <ul className="flex justify-center px-2 py-3 bg-blue-500">
+      <li className="px-2">
+        <Link href="/home" className="text-gray-300 cursor-pointer">
+          Home
+        </Link>
+      </li>
+      <li className="px-2">
+        <Link href="/about">About</Link>
+      </li>
+      <li className="px-2">
+        <Link href="/contact">Contact</Link>
+      </li>
+    </ul>
   );
 }
 
