6cb374bd57f874eed56ca77d97391dd2a691824d
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-03-11 20:46:01 +0545

    footer

---
+++ app/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-gray-600 text-center'>
+        <h2>Footer</h2>
+        <ul className='flex justify-center items-center h-12'>
+            <li className='px-3'>Facebook</li>
+            <li  className='px-3'>Instagram</li>
+            <li  className='px-3'>Discord</li>
+            <li  className='px-3'>Youtube</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Footer
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./navbar";
+import Footer from "./Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
       <Navbar />
         {children}
+      <Footer/>
       </body>
     </html>
   );
