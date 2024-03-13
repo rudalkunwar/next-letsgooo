fbd3ff48ff1fc4ee70e570f6fa504a3ba5231c75
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-03-13 19:46:24 +0545

    client and server site

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState } from "react";
 
-function page() {
+function Page() {
+  const [count, setCount] = useState(0);
+
+  const inc = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const dec = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+
   return (
-    <div className='h-screen flex items-center justify-center'>
-      <h1><i className='text-6xl'>This is home page</i></h1>
+    <div className="h-screen flex items-center justify-center">
+      <div>
+        <button className="bg-gray-400 px-3 py-2 mx-2 rounded" onClick={inc}>
+          +
+        </button>
+        {count}
+        <button className="bg-gray-400 px-3 py-2 mx-2 rounded" onClick={dec}>
+          -
+        </button>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default page
+export default Page;
